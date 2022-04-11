c75c667d7b11562036c5e06553192430ca16c401
Habil Tabesh <habil.tabesh134@gmail.com>
2022-04-10 22:50:49 -0400

    first push

---
+++ test.js
@@ -0,0 +1,32 @@
+alert("Hello, World");
+
+//not correct because of the capital letter
+Alert("Hello, World");
+
+
+//my function would not work because of the lower case f
+function myFunction() {}
+myfunction();
+
+//each statement ends with a semi-colon
+
+//spaces do not matter between the tasks
+
+//we use /* content */  for series of codes 
+
+//we use // for single line comments
+
+//use comments to divide your codes 
+
+//Js rules from top to bottom
+
+    //make the image invisible (first)
+    //move the image from top to bottom (second)
+    //make the image visible (third)
+
+//better to use camelcase 
+
+//myVar = myVar + 5      is equal to      myVar += 5        is equal to myVar
+
+//myVar  += 1        is equal to              myVar++                    ++myVar(it adds one first and then prints it)
+
